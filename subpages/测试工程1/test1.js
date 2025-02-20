765bcb98d81403bdf004946dfbfd2189a708e0e6
Ari-Hiroko <99721719+Ari-Hiroko@users.noreply.github.com>
2025-02-21 04:03:00 +0900

    测试

---
+++ subpages/测试工程1/test1.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+const minimizeButton = document.querySelector('.titlebar-button.minimize');
+const maximizeButton = document.querySelector('.titlebar-button.maximize');
+const closeButton = document.querySelector('.titlebar-button.close');
+
+minimizeButton.addEventListener('click', () => {
+  // 最小化窗口
+});
+
+maximizeButton.addEventListener('click', () => {
+  // 最大化/还原窗口
+});
+
+closeButton.addEventListener('click', () => {
+  // 关闭窗口
+});
